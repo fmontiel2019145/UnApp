6c36ae8a0974ae681521640ab97a2b4537c56089
zxdd <brian.sebasmart@gmail.com>
2020-08-30 23:14:24 -0300

    ayuda
    awebo

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>AppLink</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<script src="https://kit.fontawesome.com/8c8b9cfb6e.js" crossorigin="anonymous"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sign-in/">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body class="text-center">
+	<nav class="navbar navbar-expand navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">UnApp</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExample02">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Apps</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-md-0">
+          <input class="form-control" type="text" placeholder="Search">
+        </form>
+      </div>
+    </nav> 
+	<br><br><br>
+<div style="max-width:25%;transform: translate(150%);">
+	<form class="form-signin" method="POST" action="">
+      <h1 class="h3 mb-3 font-weight-normal">Sign in</h1>
+      <label for="inputEmail" class="sr-only">Email</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
+      <br>
+      <label for="inputPassword" class="sr-only">Contraceña</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Contraceña" required="">
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"> Recordarme
+        </label>
+      </div>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      <p class="mt-5 mb-3 text-muted">© 2020</p>
+    </form>
+</div>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ modules.php
@@ -0,0 +1,71 @@
+<?php 
+$nav = "
+<nav class=\"navbar navbar-expand navbar-dark bg-dark\">
+      <a class=\"navbar-brand\" href=\"#\">UnApp</a>
+      <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarsExample02\" aria-controls=\"navbarsExample02\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">
+        <span class=\"navbar-toggler-icon\"></span>
+      </button>
+
+      <div class=\"collapse navbar-collapse\" id=\"navbarsExample02\">
+        <ul class=\"navbar-nav mr-auto\">
+          <li class=\"nav-item active\">
+            <a class=\"nav-link\" href=\"#\">Inicio <span class=\"sr-only\">(current)</span></a>
+          </li>
+          <li class=\"nav-item\">
+            <a class=\"nav-link\" href=\"#\">Apps</a>
+          </li>
+        </ul>
+        <form class=\"form-inline my-2 my-md-0\">
+          <input class=\"form-control\" type=\"text\" placeholder=\"Search\">
+        </form>
+      </div>
+    </nav> 
+"; 
+$footer = "
+
+";
+$adminview = "
+<form class=\"form-signin\" style=\"text-aling:center;\">
+      <h1 class=\"h3 mb-3 font-weight-normal\">Sign in</h1>
+      <label for=\"inputEmail\" class=\"sr-only\">Email</label>
+      <input type=\"email\" id=\"inputEmail\" class=\"form-control\" placeholder=\"Email address\" required=\"\" autofocus=\"\">
+      <label for=\"inputPassword\" class=\"sr-only\">Contraceña</label>
+      <input type=\"password\" id=\"inputPassword\" class=\"form-control\" placeholder=\"Contraceña\" required=\"\">
+      <div class=\"checkbox mb-3\">
+        <label>
+          <input type=\"checkbox\" value=\"remember-me\"> Recordarme
+        </label>
+      </div>
+      <button class=\"btn btn-lg btn-primary btn-block\" type=\"submit\">Sign in</button>
+      <p class=\"mt-5 mb-3 text-muted\">© 2020</p>
+    </form>
+";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
